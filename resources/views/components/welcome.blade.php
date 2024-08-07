9eb2f48f67915809d36d2bb08de5d41ebaab5a23
ABLZ <ablz@MacBook-Air-de-ABLZ.local>
2024-08-07 10:19:29 -0600

    Home Dashboard correccion

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,76 +2,116 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Bienvenido a SAES - Sistema Automatizado de Evaluación de Sanciones
+        Bienvenido a SAES - Sistema de Archivo de Expedientes de Seguimiento
     </h1>
 
     <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-        El sistema SAES proporciona una plataforma robusta y fácil de usar para gestionar la evaluación y sanción de expedientes. Aquí puedes administrar eficientemente la recepción, revisión y entrega de expedientes, asegurando el cumplimiento de los plazos establecidos.
+        El Sistema de Archivo de Expedientes de Seguimiento (SAES) es una plataforma integral diseñada para gestionar eficientemente el seguimiento y archivo de expedientes. Aquí puedes administrar la recepción, revisión y seguimiento de expedientes, asegurando el cumplimiento de los plazos establecidos y manteniendo un registro detallado de todas las actividades.
     </p>
 </div>
 
 <div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-    <!-- Card: Entrega Recepción de Expedientes -->
+    <!-- Card: Recepción de Expedientes -->
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5v9h18v-9H3zm15 0V6a3 3 0 00-3-3H9a3 3 0 00-3 3v1.5M6 10.5h12M6 13.5h12M9 16.5h6" />
+            </svg>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+                Recepción de Expedientes
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            Administra la recepción de expedientes físicos y electrónicos de acuerdo a las normativas vigentes, asegurando la correcta integración y certificación de documentos.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="manual_proceso_recepcion.pdf" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                Ver Manual de Proceso
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </p>
+    </div>
+
+    <!-- Card: Seguimiento de Expedientes -->
     <div>
         <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
             <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="{{ url('/entrega-recepcion') }}">Entrega Recepción de Expedientes</a>
+                Seguimiento de Expedientes
             </h2>
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Gestiona la recepción de expedientes de acción, incluyendo la carga de archivos y programación de entregas.
+            Monitorea el estado de los expedientes, revisa las observaciones y asegura la correcta implementación de las acciones y recomendaciones emitidas.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="manual_seguimiento_expedientes.pdf" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                Ver Manual de Seguimiento
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
         </p>
     </div>
 
-    <!-- Card: Programación de Entrega -->
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="{{ url('/programacion-entrega') }}">Programación de Entrega</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Planifica y programa la entrega de expedientes a los responsables designados, asegurando el cumplimiento de los plazos establecidos.
-        </p>
-    </div>
-
-    <!-- Card: Revisión de Expedientes -->
-    <div>
-        <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-            </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="{{ url('/revision-expedientes') }}">Revisión de Expedientes</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Realiza la revisión detallada de los expedientes recibidos, verificando la integridad y completitud de la información.
-        </p>
-    </div>
-
-    <!-- Card: Devolución de Expedientes -->
+    <!-- Card: Normatividad y Documentación -->
     <div>
         <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="{{ url('/devolucion-expedientes') }}">Devolución de Expedientes</a>
+                Normatividad y Documentación
             </h2>
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Administra la devolución de expedientes que no cumplen con los requisitos para su corrección y posterior reenvío.
+            Accede a toda la documentación normativa y a los anexos necesarios para la correcta integración y revisión de los expedientes.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="normatividad.pdf" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                Ver Normatividad
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
+        </p>
+    </div>
+
+    <!-- Card: Estadísticas y Reportes -->
+    <div>
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l3 3L21.75 3" />
+            </svg>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+                Estadísticas y Reportes
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            Visualiza estadísticas detalladas y genera reportes sobre el estado y progreso de los expedientes, facilitando la toma de decisiones informadas.
+        </p>
+
+        <p class="mt-4 text-sm">
+            <a href="reportes.pdf" class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                Ver Reportes
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ms-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200">
+                    <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                </svg>
+            </a>
         </p>
     </div>
 </div>
